a90bd0365b7884803dbbfe903590c58b75dc384b
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-07-28 11:26:36 +0200

    fully functionning modal using npm

--- simple-modal-hrnet/my-project/src/lib/Modal.js
+++ simple-modal-hrnet/my-project/src/lib/Modal.js
@@ -1,25 +1,23 @@
 import React from "react";
 import "./Modal.css";
 
-
-const Modal = () => {
-
-  function closeModal() {
-    document.querySelector(".modal").classList.remove("visible");
-  }
-  
-  return (
-    <div className="modal visible">
-      <div className="modal-content">
-        <span className="modal-close" onClick={closeModal}>
-          &times;
-        </span>
-        <h2>Employee Created</h2>
+const Modal = ({ visible, onClose }) => {
+  if (visible === true) {
+    return (
+      <div className="modal">
+        <div className="modal-content">
+          <span className="modal-close" onClick={onClose}>
+            &times;
+          </span>
+          <h2>Employee Created</h2>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
+
+
 };
-
-
 
 export default Modal;
