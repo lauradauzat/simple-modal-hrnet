5d5111a233722bb0e5ebf5dccd1838a48f286a32
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-07-28 16:49:31 +0200

    properly compile with babel

--- simple-modal-hrnet/my-project/dist/Modal.js
+++ simple-modal-hrnet/my-project/dist/Modal.js
@@ -1,23 +1,27 @@
-import React from "react";
-import "./Modal.css";
+"use strict";
 
-const Modal = ({ visible, onClose }) => {
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault").default;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+require("./Modal.css");
+var Modal = function Modal(_ref) {
+  var visible = _ref.visible,
+    onClose = _ref.onClose;
   if (visible === true) {
-    return (
-      <div className="modal">
-        <div className="modal-content">
-          <span className="modal-close" onClick={onClose}>
-            &times;
-          </span>
-          <h2>Employee Created</h2>
-        </div>
-      </div>
-    );
+    return /*#__PURE__*/_react.default.createElement("div", {
+      className: "modal"
+    }, /*#__PURE__*/_react.default.createElement("div", {
+      className: "modal-content"
+    }, /*#__PURE__*/_react.default.createElement("span", {
+      className: "modal-close",
+      onClick: onClose
+    }, "\xD7"), /*#__PURE__*/_react.default.createElement("h2", null, "Employee Created")));
   } else {
     return null;
   }
-
-
 };
-
-export default Modal;
+var _default = Modal;
+exports.default = _default;
--- simple-modal-hrnet/my-project/dist/index.js
+++ simple-modal-hrnet/my-project/dist/index.js
@@ -1,2 +1,13 @@
-import Modal from "./Modal";
-export { Modal };
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault").default;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Modal", {
+  enumerable: true,
+  get: function get() {
+    return _Modal.default;
+  }
+});
+var _Modal = _interopRequireDefault(require("./Modal"));
